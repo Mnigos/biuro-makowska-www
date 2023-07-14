c4fd20d75f715ba5a9926d2028559e183b269cc0
Mnigos <makowskiigor@gmail.com>
2023-07-14 11:37:55 +0200

    feat(assets): add images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,3 @@
+export { default as background } from './background.jpg'
+export { default as biuroMakowskaLogo } from './biuro-makowska-logo.png'
+export { default as kibrLogo } from './kibr-logo.png'
