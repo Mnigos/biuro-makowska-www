c002629006b436c6030d8707770643c0c9535f79
Mnigos <makowskiigor@gmail.com>
2023-07-14 11:43:42 +0200

    feat(pages/_app): implement layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,22 @@ import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
 import '~/styles/theme.css'
+import '~/styles/global.css'
+
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Layout, LayoutType } from '~/layouts'
+
+export type PageProps = {
+  layoutType: LayoutType
+}
+
+export default function App({ Component, pageProps }: AppProps<PageProps>) {
+  const { layoutType = LayoutType.DEFAULT } = pageProps
+
+  return (
+    <Layout type={layoutType}>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
