d476f844eabe587689813c3db1e81eb370b1b7c4
Mnigos <makowskiigor@gmail.com>
2023-07-15 19:35:17 +0200

    feat(components): create footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import { Divider } from 'primereact/divider'
+import { classNames } from 'primereact/utils'
+import { HTMLAttributes } from 'react'
+
+export function Footer({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <footer className={classNames('max-w-screen px-2', className)} {...props}>
+      <Divider className="surface-100" style={{ height: '1px' }} />
+
+      <p className="p-component w-full text-center">
+        Igor Makowski &copy; {new Date().getFullYear()}
+      </p>
+    </footer>
+  )
+}
