22dc24c34b6c486c9b8dd83512e715176f482d69
Mnigos <makowskiigor@gmail.com>
2023-07-15 19:40:20 +0200

    refactor(api/utils): remove unnecesary `instanceOfLandingPage` type util

--- src/components/cards/about-company.tsx
+++ src/components/cards/about-company.tsx
@@ -4,12 +4,11 @@ import ReactMarkdown from 'react-markdown'
 import { cardPassThrough } from '.'
 
 import { useAboutCompany } from '~/hooks/api'
-import { instanceOfLandingPage } from '~/api/utils'
 
 export function AboutCompanyCard() {
   const { data } = useAboutCompany()
 
-  if (!data || !instanceOfLandingPage(data)) return null
+  if (!data) return null
 
   return (
     <Card title={data.header} pt={cardPassThrough} className="lg:w-6">
