1b6dc97ad42ecfabc0aa574d0720c7dc4d0fc51f
Mnigos <makowskiigor@gmail.com>
2023-07-15 11:22:52 +0200

    refactor(pages): move landing page cards to separate components

---
+++ src/components/cards/index.ts
@@ -0,0 +1,2 @@
+export * from './about-company'
+export * from './welcome-to-my-page'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,30 @@
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
-import { Card } from 'primereact/card'
+import { QueryClient, dehydrate } from '@tanstack/react-query'
 
 import { PageProps } from './_app'
 
 import { biuroMakowskaLogo, kibrLogo } from '~/assets/images'
 import { LayoutType } from '~/layouts'
+import { getAboutCompany, getWelcomeToMyPage } from '~/api/fetchers'
+import { AboutCompanyCard, WelcomeToMyPageCard } from '~/components/cards'
+import { ABOUT_COMPANY, WELCOME_TO_MY_PAGE } from '~/api/constants'
 
-export const getStaticProps: GetStaticProps<PageProps> = async () => ({
-  props: {
-    layoutType: LayoutType.LANDING,
-  },
-})
+export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery([ABOUT_COMPANY], () => getAboutCompany())
+  await queryClient.prefetchQuery([WELCOME_TO_MY_PAGE], () =>
+    getWelcomeToMyPage()
+  )
+
+  return {
+    props: {
+      layoutType: LayoutType.LANDING,
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
+}
 
 export default function Home() {
   return (
@@ -27,68 +40,18 @@ export default function Home() {
           className="max-w-full"
         />
 
-        <Card title="Witam na mojej stronie" className="lg:w-5">
-          <p>
-            Proponuję Państwu profesjonalne usługi księgowe w atrakcyjnych
-            cenach z możliwością odbioru dokumentów od klienta.
-          </p>
-
-          <p> Posiadam:</p>
-          <ul>
-            <li>wieloletnie doświadczenie w obsłudze księgowej firm</li>
-            <li>ubezpieczenie w zakresie odpowiedzialności cywilnej</li>
-            <li>licencję Ministra Finansów</li>
-          </ul>
-        </Card>
+        <WelcomeToMyPageCard />
       </div>
 
       <div className="flex flex-column lg:flex-row justify-content-between align-items-center gap-4">
-        <Card title="O Firmie" className="lg:w-12 xl:w-8">
-          <p>
-            Biuro Rachunkowe Joanna Makowska to certyfikowane biuro, które
-            zapewnia swoim klientom profesjonalną obsługę oraz rzetelne i
-            fachowe świadczenie usług księgowych.
-          </p>
+        <AboutCompanyCard />
 
-          <p>
-            Wieloletnia praca zawodowa, nabyta wiedza teoretyczna i praktyczna
-            oraz umiejętności jej wykorzystania to dowód na rzetelną realizację
-            usług.
-          </p>
-
-          <p>
-            Działalność biura rachunkowego to dziś istotna pomoc dla wielu
-            przedsiębiorców. Chcemy wychodzić naprzeciw wymaganiom i
-            oczekiwaniom, które pojawiają się ze strony przedsiębiorców, dlatego
-            oferujemy szeroki zakres usług księgowych i kadrowych w ramach
-            prowadzenia KPiR, ksiąg rachunkowych, obsługi płac pracowniczych,
-            deklaracji podatkowych, ZUS, kontaktów z Urzędem Skarbowy, rozliczeń
-            rocznych PIT oraz pomocy przy działalności gospodarczej.
-          </p>
-
-          <p>
-            Świadczone przez nas usługi księgowe i kadrowe realizujemy na
-            podstawie certyfikatu księgowego wydanego przez Ministra Finansów nr
-            26183/01. Posiadamy obowiązkowe i dodatkowe ubezpieczenie OC.
-          </p>
-
-          <p>
-            Jednocześnie stale poszerzamy swoją merytoryczną wiedzę uczestnicząc
-            na bieżąco w organizowanych szkoleniach, chociażby w
-            Stowarzyszeniach Księgowych w Polsce. Serdecznie zapraszamy Państwa
-            do skorzystania z naszych kompleksowych i elastycznie dopasowanych
-            do Państwa potrzeb usług.
-          </p>
-        </Card>
-
-        <div className="flex justify-content-center">
-          <Image
-            src={kibrLogo}
-            alt="Kibr's logo"
-            style={{ objectFit: 'contain' }}
-            className="w-full md:w-4 lg:w-8"
-          />
-        </div>
+        <Image
+          src={kibrLogo}
+          alt="Kibr's logo"
+          style={{ objectFit: 'contain' }}
+          className="w-full md:w-4 lg:w-4"
+        />
       </div>
     </>
   )
