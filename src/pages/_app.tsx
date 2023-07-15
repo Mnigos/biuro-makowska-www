99a6a3fa0e5349c302e28baaf745617e9eb2e9f9
Mnigos <makowskiigor@gmail.com>
2023-07-15 11:22:20 +0200

    feat(pages/_app): initialize react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,19 +6,35 @@ import '~/styles/theme.css'
 import '~/styles/global.css'
 
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { Layout, LayoutType } from '~/layouts'
 
 export type PageProps = {
-  layoutType: LayoutType
+  layoutType?: LayoutType
+  dehydratedState?: unknown
 }
 
 export default function App({ Component, pageProps }: AppProps<PageProps>) {
-  const { layoutType = LayoutType.DEFAULT } = pageProps
+  const [queryClient] = useState(() => new QueryClient())
+
+  const { layoutType = LayoutType.DEFAULT, dehydratedState } = pageProps
 
   return (
-    <Layout type={layoutType}>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <Layout type={layoutType}>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
