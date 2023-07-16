4ac0a456b6645516a6d7896d9b5661d6a3d49c1b
Mnigos <makowskiigor@gmail.com>
2023-07-16 15:54:01 +0200

    fix(api/adapters/prices-cards-response): sort data

--- src/api/adapters/prices-cards-response.ts
+++ src/api/adapters/prices-cards-response.ts
@@ -3,14 +3,16 @@ import { PricesCard, PricesCardsResponse } from '../types'
 export const adaptPricesCardsResponse = ({
   data,
 }: PricesCardsResponse): PricesCard[] => {
-  return data.map(({ attributes: { tables, header, ...rest } }) => ({
-    tables: tables.map(({ rows, ...rest }) => ({
-      rows: rows.map(({ cells }) =>
-        Object.fromEntries(cells.map(({ data }, index) => [index, data]))
-      ),
+  return data
+    .sort(({ id: firstId }, { id: secondId }) => firstId - secondId)
+    .map(({ attributes: { tables, header, ...rest } }) => ({
+      tables: tables.map(({ rows, ...rest }) => ({
+        rows: rows.map(({ cells }) =>
+          Object.fromEntries(cells.map(({ data }, index) => [index, data]))
+        ),
+        ...rest,
+      })),
+      header,
       ...rest,
-    })),
-    header,
-    ...rest,
-  }))
+    }))
 }
