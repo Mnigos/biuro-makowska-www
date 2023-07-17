d67e95ff90682a34ab070c299fedba5f9417366e
Mnigos <makowskiigor@gmail.com>
2023-07-17 12:48:44 +0200

    feat(assets/images): add images for contact page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,6 @@
 export { default as background } from './background.jpg'
 export { default as biuroMakowskaLogo } from './biuro-makowska-logo.png'
 export { default as kibrLogo } from './kibr-logo.png'
+export { default as biuroMakowskaOfficeOutside } from './biuro-makowska-office-outside.jpg'
+export { default as biuroMakowskaOfficeInside } from './biuro-makowska-office-inside.jpeg'
+export { default as joannaMakowskaPhoto } from './joanna-makowska-photo.jpg'
